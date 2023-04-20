ea0d3a74198da1e6e434ab765fac00fab85f4d16
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-04-20 12:22:36 +0100

    Add AuthenticationFilter to cloud gateway routes
    This commit adds an AuthenticationFilter to the cloud gateway configuration for both the user-service and account-service routes. The filter is responsible for performing authentication or authorization by validating the Bearer token on incoming requests before they are routed to the appropriate backend service.
    
    The purpose of this change is to enhance the security of the cloud gateway by adding a shared filter that can perform consistent authentication checks across multiple routes. By doing so, we can reduce the likelihood of security vulnerabilities and improve the overall reliability of the system.

---
+++ src/main/java/com/amyojiakor/apigateway/filter/RouteValidator.java
@@ -0,0 +1,21 @@
+package com.amyojiakor.apigateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/login"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
