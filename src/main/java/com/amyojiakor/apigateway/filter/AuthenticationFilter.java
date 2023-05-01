0e476d5c9342acccfd541c46608f26181fa8e83e
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-05-01 22:27:54 +0100

    Add JWT token header to request and update global CORS configuration
    This commit adds two lines of code to the `apply()` method of the gateway filter, which mutate the `ServerHttpRequest` to include the JWT token header when present. It also updates the properties file to configure global CORS for allowed origins, methods, and headers. This allows clients to make authorized requests to the API gateway with CORS enabled.

--- src/main/java/com/amyojiakor/apigateway/filter/AuthenticationFilter.java
+++ src/main/java/com/amyojiakor/apigateway/filter/AuthenticationFilter.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
 import java.util.Objects;
 
@@ -31,23 +33,28 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                     throw new RuntimeException("missing authorization header");
                 }
                 String authHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
-//                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
                 if (authHeader != null && authHeader.startsWith("Bearer ")) {
                     authHeader = authHeader.substring(7);
+                    System.out.println(authHeader);
                 }
-
                 try {
                     jwtUtil.validateToken(authHeader);
                 } catch (Exception e) {
+                    System.out.println(authHeader);
                     System.out.println("invalid access...!");
                     throw new RuntimeException("unauthorized access to application");
                 }
+                ServerHttpRequest request = exchange.getRequest().mutate()
+                        .header(HttpHeaders.AUTHORIZATION, authHeader)
+                        .build();
+                ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
+                System.out.println(authHeader);
+                return chain.filter(mutatedExchange);
             }
             return chain.filter(exchange);
         });
     }
 
     public static class Config {
-
     }
 }
--- src/main/java/com/amyojiakor/apigateway/util/JwtUtil.java
+++ src/main/java/com/amyojiakor/apigateway/util/JwtUtil.java
@@ -14,11 +14,16 @@ public class JwtUtil {
     public static final String SECRET = "462948404D635166546A576E5A7234753778214125442A472D4B614E64526755";
 
     public void validateToken(final String token) {
-        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+        Jwts
+            .parserBuilder()
+            .setSigningKey(getSignKey())
+            .build()
+            .parseClaimsJws(token);
     }
 
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
 }
